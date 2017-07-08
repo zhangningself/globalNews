29dd40f01fca1b739b1317e4a20186dac91b73be
zhangningself <842675998@qq.com>
2017-07-08 14:01:28 +0800

    header

--- globalnews-admin/src/components/Header/header.vue
+++ globalnews-admin/src/components/Header/header.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="zq-header">
-    <h1>this is header</h1>
+  <div id="zn-header">
+    <h1>
+	    <a href="##">
+	    	<img src="../../img/logo.png"/>
+	    	
+	    </a>
+	    <img class="logo2" src="../../img/logo2.png"/>
+    </h1>
+    
+    <div class="right">
+    	hello!
+    	<span>admin1</span>
+    </div>
   </div>
 </template>
 
@@ -9,9 +20,35 @@
 </script>
 
 <style lang="scss">
-  #zq-header{
-    width:100%;
-    height:80px;
-    background:red;
+  #zn-header{
+  	margin: 0 auto;
+    width:1200px;
+    height:100px;
+    background:#fff;
+    position: relative;
+    h1{
+    	position: absolute;
+    	top: 10px;
+    	left: 5px;
+    	a img{
+    		width: 80px;
+    	}
+    	.logo2{
+    		position: absolute;
+    	top: 10px;
+    	left: 95px;
+    	}
+    }
+    .right{
+    		float: right;
+    		padding: 30px;
+    		right:0px;
+    		top: 10px;
+    		color: #2e2e2e;
+    		font-size: 20px;
+    		span{
+    			color: #FF6C60;
+    		}
+    }
   }
 </style>
